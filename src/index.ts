15a2697805153a59e9ac89a1324eb80bd5cc2d72
Gn Gn <gngenius02@gmail.com>
2020-04-18 15:51:21 -0700

    inital

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import { port } from "./env";
+import path from "path";
+import morgan from "morgan";
+import helmet from "helmet";
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.get("/t", (req, res) => {
+  res.sendFile(path.resolve("./outs/t.js"));
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
